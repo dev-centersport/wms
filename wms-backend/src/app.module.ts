16f2389d3bf743585ad3e022a5099f171e3797b6
Centersport <desenvolvimento@centersport.com.br>
2025-06-24 11:35:40 -0300

    ambiente concluído

--- wms-backend/src/app.module.ts
+++ wms-backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5433,
+  username: 'admin',
+  password: 'senha123',
+  database: 'wms_db',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true, // ATENÇÃO: usar apenas em desenvolvimento
+  logging: true,
+};
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
