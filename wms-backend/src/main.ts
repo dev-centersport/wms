7e32de5ce5e9ef058c71e90b4b4f13ecdc4471f8
Centersport <desenvolvimento@centersport.com.br>
2025-07-01 17:19:57 -0300

    Instalação do hbs para primeiras localizações

--- wms-backend/src/main.ts
+++ wms-backend/src/main.ts
@@ -1,20 +1,25 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express'; // Add this import
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Specify NestExpressApplication as the type parameter
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Configuração global do ValidationPipe
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // remove propriedades não incluídas no DTO
-      forbidNonWhitelisted: true, // lança erro se tiver propriedades não permitidas
-      transform: true, // transforma os tipos automaticamente (ex: string para number)
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
