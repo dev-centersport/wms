22428eff151ba9158372d8f997d1d8cf3b78fe09
dev-centersport <desenvolvimento@centersport.com.br>
2025-07-04 08:43:06 -0300

    Update Armazem.tsx

--- wms-frontend/src/pages/Armazem.tsx
+++ wms-frontend/src/pages/Armazem.tsx
@@ -1,23 +1,64 @@
 import React from 'react';
 import {
+<<<<<<< Updated upstream
   TableBody,
   TableCell,
   TableRow,
 } from '@mui/material';
+=======
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from '@mui/material';
+import {
+  Search as SearchIcon,
+  Delete as DeleteIcon,
+  Print as PrintIcon,
+  List as ListIcon,
+  Add as AddIcon,
+  FilterList as FilterListIcon,
+} from '@mui/icons-material';
+>>>>>>> Stashed changes
 import { useLocalizacoes } from '../components/ApiComponents';
 
 
 const Armazem: React.FC = () => {
   // Hook personalizado que gerencia a lógica de localizações
   const {
+<<<<<<< Updated upstream
     locaisFiltrados,
+=======
+    listaLocalizacoes,
+    locaisFiltrados,
+    busca,
+    setBusca,
+    mostrarFormulario,
+    setMostrarFormulario,
+    mostrarFiltro,
+    setMostrarFiltro,
+    setListaLocalizacoes,
+>>>>>>> Stashed changes
   } = useLocalizacoes();
 
   return (
     <TableBody>
       {locaisFiltrados.length ? (
         locaisFiltrados.map((item, index) => (
+<<<<<<< Updated upstream
           <TableRow key={`${item.nome}-${index}`}>
+=======
+          <TableRow key={`${item.localizacao}-${index}`}>
+>>>>>>> Stashed changes
             <TableCell>{item.armazem}</TableCell>
             <TableCell>{item.capacidade}</TableCell>
             <TableCell>{item.endereco}</TableCell>
