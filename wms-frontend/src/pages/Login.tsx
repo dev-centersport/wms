488876b267b62ee71d1dfd9963fde98d6db3b185
dev-centersport <desenvolvimento@centersport.com.br>
2025-07-08 09:20:45 -0300

    LoginEstilizacao

--- wms-frontend/src/pages/Login.tsx
+++ wms-frontend/src/pages/Login.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FaUser } from "react-icons/fa";
-import logo from "../img/image.png"
+import { FaUser, FaEye, FaEyeSlash } from "react-icons/fa";
+import logo from "../img/image.png";
 
 const IconUser = FaUser as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
+const IconEye = FaEye as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
+const IconEyeSlash = FaEyeSlash as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
   const [usuario, setUsuario] = useState("");
   const [senha, setSenha] = useState("");
+  const [mostrarSenha, setMostrarSenha] = useState(false);
 
   const handleLogin = () => {
     navigate("/armazem");
@@ -16,7 +19,7 @@ const Login: React.FC = () => {
 
   return (
     <div style={{
-      backgroundColor: "#55F136",
+      backgroundColor: "rgb(97, 222, 37)",
       height: "100vh",
       display: "flex",
       justifyContent: "center",
@@ -26,31 +29,31 @@ const Login: React.FC = () => {
         backgroundColor: "transparent",
         padding: "3rem",
         borderRadius: "18px",
-        width: "400px",
+        width: "590px",
         border: "3px solid white",
         textAlign: "center"
       }}>
 
         <div style={{ marginBottom: "1.5rem" }}>
           <div style={{
-            backgroundColor: "#55F136",
-            borderRadius: "50%",
-            width: "150px",
-            height: "150px",
-            margin: "0 auto",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            boxShadow: "0 0 25px rgba(0, 0, 0, 0.3)"
+            backgroundColor: "rgb(97, 222, 37)",
           }}>
-            <img src={logo} alt="Logo WMS" style={{ width: "100px", height: "100px" }} />
+            <img
+              src={logo}
+              alt="Logo WMS"
+              style={{
+                width: "80%",
+                height: "80%",
+                objectFit: "contain"
+              }}
+            />
           </div>
           <h2 style={{
-            margin: "15px 0 5px",
+            margin: "15px 0 0px",
             fontWeight: "bold",
             fontSize: "32px"
-          }}>WMS</h2>
-          <p style={{ fontSize: "16px" }}>Bem Vindo!</p>
+          }}></h2>
+          <p style={{ fontSize: "23px", fontWeight: "bold", marginTop: -20 }}>Bem Vindo!</p>
         </div>
 
         <div style={{ marginBottom: "1rem", textAlign: "left" }}>
@@ -63,7 +66,7 @@ const Login: React.FC = () => {
             marginTop: "5px"
           }}>
             <div style={{ padding: "0.75rem" }}>
-              <IconUser color="#55F136" />
+              <IconUser color="rgb(97, 222, 37)" />
             </div>
             <input
               type="text"
@@ -83,19 +86,45 @@ const Login: React.FC = () => {
 
         <div style={{ marginBottom: "1.5rem", textAlign: "left" }}>
           <label style={{ fontWeight: "bold" }}>Senha</label>
-          <input
-            type="password"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            style={{
-              width: "100%",
-              padding: "0.75rem",
-              borderRadius: "10px",
-              border: "none",
-              fontSize: "1rem",
-              marginTop: "5px"
-            }}
-          />
+          <div style={{
+            display: "flex",
+            alignItems: "center",
+            backgroundColor: "white",
+            borderRadius: "10px",
+            marginTop: "5px"
+          }}>
+            <input
+              type={mostrarSenha ? "text" : "password"}
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              style={{
+                border: "none",
+                outline: "none",
+                flex: 1,
+                padding: "0.75rem",
+                background: "transparent",
+                fontSize: "1rem"
+              }}
+            />
+            <button
+              type="button"
+              onClick={() => setMostrarSenha(!mostrarSenha)}
+              style={{
+                border: "none",
+                background: "transparent",
+                padding: "0.75rem",
+                cursor: "pointer"
+              }}
+              aria-label={mostrarSenha ? "Ocultar senha" : "Mostrar senha"}
+            >
+              {mostrarSenha ? (
+                <IconEyeSlash color="rgb(97, 222, 37)" style={{ fontSize: "1.25rem" }} />
+              ) : (
+                <IconEye color="rgb(97, 222, 37)" style={{ fontSize: "1.25rem" }} />
+              )}
+            </button>
+
+          </div>
         </div>
 
         <button
@@ -108,16 +137,18 @@ const Login: React.FC = () => {
             border: "none",
             borderRadius: "8px",
             fontWeight: "bold",
-            fontSize: "1rem"
+            fontSize: "1rem",
+            marginBottom: "30px"
           }}
         >
           Entrar
         </button>
 
         <p style={{
-          marginTop: "1.5rem",
+          marginBottom: -20,
           fontStyle: "italic",
-          fontSize: "0.9rem"
+          fontWeight: "bold",
+          fontSize: "1.02rem"
         }}>
           "Otimizando a gestão de armazém com tecnologia eficiente"
         </p>
