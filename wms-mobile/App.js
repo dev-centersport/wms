ec4a2d4130f33b9b25897feee05572792cc7b93c
dev-centersport <desenvolvimento@centersport.com.br>
2025-07-23 10:39:41 -0300

    tirarabarrabrancedelogin

--- wms-mobile/App.js
+++ wms-mobile/App.js
@@ -28,7 +28,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="Home"
           component={Home}
           options={{ headerShown: false }} />
