c132337ed9e8368b7af8be119bedb88742c7aed7
Centersport <desenvolvimento@centersport.com.br>
2025-08-05 10:41:16 -0300

    Update agrupamento.entity.ts

--- wms-backend/src/agrupamento/entities/agrupamento.entity.ts
+++ wms-backend/src/agrupamento/entities/agrupamento.entity.ts
@@ -1,1 +1,13 @@
-export class Agrupamento {}
+import { Column, Entity, Polygon, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Agrupamento {
+  @PrimaryGeneratedColumn()
+  agrupamento_id: number;
+
+  @Column({ type: 'varchar' })
+  nome: string;
+
+  @Column({ type: 'geometry', spatialFeatureType: 'Polygon', srid: 4326 })
+  geometry: Polygon;
+}
